74277290a87c5a3cd58d8cac7a21c6d503cb541f
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-04 23:53:41 +0300

    enable treesitter based indenting & incremental selection

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,15 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = { 'astro', 'bash', 'c', 'c_sharp', 'cmake', 'cpp', 'css', 'dart', 'dockerfile', 'elixir', 'fish', 'gdscript', 'gitignore', 'glsl', 'go', 'godot_resource', 'gomod', 'graphql', 'help', 'hlsl', 'hocon', 'html', 'java', 'javascript', 'jsdoc', 'json', 'json5', 'jsonc', 'kotlin', 'latex', 'llvm', 'lua', 'make', 'markdown', 'markdown_inline', 'meson', 'ninja', 'nix', 'perl', 'php', 'phpdoc', 'prisma', 'proto', 'pug', 'python', 'qmljs', 'query', 'rasi', 'rst', 'ruby', 'rust', 'scala', 'scss', 'sql', 'svelte', 'swift', 'toml', 'tsx', 'typescript', 'vim', 'vue', 'yaml', 'zig' },
-  highlight = {
+  highlight = { enable = true },
+  indent = { enable = true },
+  incremental_selection = {
     enable = true,
+    keymaps = {
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      scope_incremental = '<c-s>',
+      node_decremental = '<c-backspace>'
+    }
   }
 }
 
