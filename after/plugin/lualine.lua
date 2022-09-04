f771414ee40989bb18be80dc9a05d9917e961544
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-05 00:37:11 +0300

    enable winbar only on nightly

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,22 +1,24 @@
+local nvim080 = vim.fn.has('nvim-0.8.0') == 1
+
 require('lualine').setup {
   options = {
     globalstatus = true
   },
-  winbar = {
+  winbar = nvim080 and {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {'filename'},
     lualine_x = {},
     lualine_y = {},
     lualine_z = {}
-  },
-  inactive_winbar = {
+  } or nil,
+  inactive_winbar = nvim080 and {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {'filename'},
     lualine_x = {},
     lualine_y = {},
     lualine_z = {}
-  },
+  } or nil,
   extensions = { 'nvim-tree' }
 }
