bf282a4a41dcd765dd3c5f3a5e7f7f39fe53e441
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-07 01:06:35 +0300

    update packer setup code to newer style

--- lua/ozokuz/plugins.lua
+++ lua/ozokuz/plugins.lua
@@ -1,17 +1,24 @@
 -- Setup Packer if not installed
-local fn = vim.fn
-local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system {
-    'git',
-    'clone',
-    '--depth',
-    '1',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path,
-  }
-  vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath 'data'
+    .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system {
+      'git',
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/wbthomason/packer.nvim',
+      install_path,
+    }
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 -- Setup plugins
 return require('packer').startup(function(use)
