5c0309c360ec07d14afc3f2a5f5357ef68065e62
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-08 19:03:45 +0300

    hacky automatic file explorer closing when closing all other buffers in a tab

--- after/plugin/file-explorer.lua
+++ after/plugin/file-explorer.lua
@@ -17,6 +17,27 @@ require('nvim-tree').setup {
   open_on_tab = true,
 }
 
+-- TODO: bugs out when opening plugin windows when only NvimTree is open
+vim.o.confirm = true
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = vim.api.nvim_create_augroup('NvimTreeClose', { clear = true }),
+  callback = function()
+    local layout = vim.api.nvim_call_function('winlayout', {})
+
+    if layout[1] ~= 'leaf' then
+      return
+    end
+
+    local buffer = vim.api.nvim_win_get_buf(layout[2])
+    local filetype = vim.api.nvim_buf_get_option(buffer, 'filetype')
+    local is_nvim_tree = filetype == 'NvimTree'
+
+    if is_nvim_tree and layout[3] == nil then
+      vim.cmd 'quit'
+    end
+  end,
+})
+
 vim.keymap.set(
   'n',
   '<leader>ff',
