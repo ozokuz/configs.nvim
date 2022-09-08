928420f84854a1581ceb11f48b55be7df483b3fd
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-08 16:27:50 +0300

    turned packer window into a floating window

--- lua/ozokuz/plugins.lua
+++ lua/ozokuz/plugins.lua
@@ -21,86 +21,95 @@ end
 local packer_bootstrap = ensure_packer()
 
 -- Setup plugins
-return require('packer').startup(function(use)
-  -- Manage itself
-  use 'wbthomason/packer.nvim'
-  -- The Lodash of Neovim
-  use 'nvim-lua/plenary.nvim'
-  -- Icons
-  use 'kyazdani42/nvim-web-devicons'
-  -- Colorscheme
-  use 'folke/tokyonight.nvim'
-  -- Tmux Integration
-  use 'alexghergh/nvim-tmux-navigation'
-  -- Statusline
-  use 'nvim-lualine/lualine.nvim'
-  -- Tabline
-  use { 'akinsho/bufferline.nvim', tag = 'v2.*' }
-  -- File Explorer
-  use 'kyazdani42/nvim-tree.lua'
-  -- Floating Terminal
-  use 'voldikss/vim-floaterm'
-  -- Show keybinds
-  use 'folke/which-key.nvim'
-  -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      require('nvim-treesitter.install').update { with_sync = true }
-    end,
-    requires = {
-      'p00f/nvim-ts-rainbow',
-      'nvim-treesitter/nvim-treesitter-context',
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-  }
-  -- LSP
-  use {
-    'glepnir/lspsaga.nvim',
-    branch = 'main',
-    requires = {
-      'lewis6991/hover.nvim',
-      'stevearc/aerial.nvim',
-      'j-hui/fidget.nvim',
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'jose-elias-alvarez/null-ls.nvim',
-      'neovim/nvim-lspconfig',
-    },
-  }
-  -- Language specific plugins
-  use 'folke/lua-dev.nvim'
-  -- Completion
-  use {
-    'hrsh7th/nvim-cmp',
-    requires = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-emoji',
-      'hrsh7th/cmp-cmdline',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'onsails/lspkind.nvim',
-    },
-  }
-  -- Commenting
-  use 'numToStr/Comment.nvim'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
-  -- Doc Strings
-  use 'danymat/neogen'
-  -- Colorizing
-  use 'NvChad/nvim-colorizer.lua'
-  use 'mrshmllow/document-color.nvim'
-  -- Git Signs
-  use 'lewis6991/gitsigns.nvim'
-  -- Blank line indents
-  use 'lukas-reineke/indent-blankline.nvim'
-  -- Automatic Indentation
-  use 'tpope/vim-sleuth'
+return require('packer').startup {
+  function(use)
+    -- Manage itself
+    use 'wbthomason/packer.nvim'
+    -- The Lodash of Neovim
+    use 'nvim-lua/plenary.nvim'
+    -- Icons
+    use 'kyazdani42/nvim-web-devicons'
+    -- Colorscheme
+    use 'folke/tokyonight.nvim'
+    -- Tmux Integration
+    use 'alexghergh/nvim-tmux-navigation'
+    -- Statusline
+    use 'nvim-lualine/lualine.nvim'
+    -- Tabline
+    use { 'akinsho/bufferline.nvim', tag = 'v2.*' }
+    -- File Explorer
+    use 'kyazdani42/nvim-tree.lua'
+    -- Floating Terminal
+    use 'voldikss/vim-floaterm'
+    -- Show keybinds
+    use 'folke/which-key.nvim'
+    -- Treesitter
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+        require('nvim-treesitter.install').update { with_sync = true }
+      end,
+      requires = {
+        'p00f/nvim-ts-rainbow',
+        'nvim-treesitter/nvim-treesitter-context',
+        'nvim-treesitter/nvim-treesitter-textobjects',
+      },
+    }
+    -- LSP
+    use {
+      'glepnir/lspsaga.nvim',
+      branch = 'main',
+      requires = {
+        'lewis6991/hover.nvim',
+        'stevearc/aerial.nvim',
+        'j-hui/fidget.nvim',
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'jose-elias-alvarez/null-ls.nvim',
+        'neovim/nvim-lspconfig',
+      },
+    }
+    -- Language specific plugins
+    use 'folke/lua-dev.nvim'
+    -- Completion
+    use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-emoji',
+        'hrsh7th/cmp-cmdline',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'onsails/lspkind.nvim',
+      },
+    }
+    -- Commenting
+    use 'numToStr/Comment.nvim'
+    use 'JoosepAlviste/nvim-ts-context-commentstring'
+    -- Doc Strings
+    use 'danymat/neogen'
+    -- Colorizing
+    use 'NvChad/nvim-colorizer.lua'
+    use 'mrshmllow/document-color.nvim'
+    -- Git Signs
+    use 'lewis6991/gitsigns.nvim'
+    -- Blank line indents
+    use 'lukas-reineke/indent-blankline.nvim'
+    -- Automatic Indentation
+    use 'tpope/vim-sleuth'
 
-  -- If packer was just installed then sync plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+    -- If packer was just installed then sync plugins
+    if packer_bootstrap then
+      require('packer').sync()
+    end
+  end,
+  config = {
+    display = {
+      open_fn = function()
+        return require('packer.util').float { border = 'single' }
+      end,
+    },
+  },
+}
