dfb02dd18705872fcf5c7a61401356c4de0a32ea
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-09 13:15:03 +0300

    clean up utils

--- lua/ozokuz/core/utils.lua
+++ lua/ozokuz/core/utils.lua
@@ -1,15 +1,11 @@
 local M = {}
 
-function M.has(x)
-  return vim.fn.has(x) == 1
-end
-
 function M.is_win()
-  return M.has 'win32'
+  return vim.fn.has 'win32' == 1
 end
 
 function M.is_nvim_080()
-  return M.has 'nvim-0.8.0'
+  return vim.version().minor == 8
 end
 
 return M
